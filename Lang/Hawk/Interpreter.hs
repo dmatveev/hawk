84b7dc5b9e2fd1ed93eaf526f9420c895b07dc8a
Dmitry Matveev <me@dmitrymatveev.co.uk>
2014-10-03 21:55:00 +0400

    Basic arrays implementation
    * Can be assigned and modified
    * All arrays in the program are actually stored in a single Data.Map
      (with a compound key)

--- Lang/Hawk/Interpreter.hs
+++ Lang/Hawk/Interpreter.hs
@@ -11,7 +11,7 @@ import GHC.Float (floatToDigits)
 import Control.Monad.State.Strict
 import Control.Monad.Cont
 import Control.Monad.Trans
-import qualified Data.Map.Strict as Map
+import qualified Data.Map.Strict as M
 
 import Lang.Hawk.AST
 
@@ -21,18 +21,20 @@ data Value = VString !B.ByteString
 
 data HawkContext = HawkContext
                  { hcCode     :: !AwkSource
-                 , hcFields   :: !(Map.Map Int Value)
-                 , hcVars     :: !(Map.Map String Value)
-                 , hcBVars    :: !(Map.Map String Value)
+                 , hcFields   :: !(M.Map Int Value)
+                 , hcVars     :: !(M.Map String Value)
+                 , hcArrays   :: !(M.Map (String, String) Value)
+                 , hcBVars    :: !(M.Map String Value)
                  , hcThisLine :: !B.ByteString
                  }
 
 emptyContext :: AwkSource -> HawkContext
 emptyContext s = HawkContext
                  { hcCode     = s
-                 , hcFields   = Map.empty
-                 , hcVars     = Map.fromList initialVars
-                 , hcBVars    = Map.fromList initialBuiltInVars
+                 , hcFields   = M.empty
+                 , hcVars     = M.fromList initialVars
+                 , hcArrays   = M.empty
+                 , hcBVars    = M.fromList initialBuiltInVars
                  , hcThisLine = ""
                  }
   where initialVars = map (, VString "") $ vars s 
@@ -103,7 +105,7 @@ processLine :: KBlock -> B.ByteString -> Interpreter ()
 processLine k s = do
     oldContext <- get
     let thisFields = map VString $ B.words s
-        thisFldMap = Map.fromList (zip [1,2..] thisFields)
+        thisFldMap = M.fromList (zip [1,2..] thisFields)
         thisContext = oldContext { hcThisLine = s
                                  , hcFields   = thisFldMap
                                  }
@@ -158,22 +160,27 @@ eval (FieldRef e) = do
      if i == 0
      then gets hcThisLine >>= (return . VString)
      else do fs <- gets hcFields
-             return $! fs Map.! i
+             return $! fs M.! i
 
 eval (VariableRef s) = do
      vs <- gets hcVars
-     return $! vs Map.! s
+     return $! vs M.! s
 
 eval (BuiltInVar s) = do
      bvs <- gets hcBVars
-     return $! bvs Map.! s
+     return $! bvs M.! s
 
-eval (ArrayRef _ _) = unsup "Array references"
+eval (ArrayRef s e) = do
+     idx <- liftM toString $! eval e
+     ars <- gets hcArrays
+     return $! ars M.! (s, B.unpack idx)
 
 eval (Incr n f@(FieldRef e))    = incrField n f
-eval (Incr n v@(VariableRef s)) = incrVar n v
+eval (Incr n v@(VariableRef s)) = incrVar   n v
+eval (Incr n a@(ArrayRef s e))  = incrArr   n a
 eval (Decr n f@(FieldRef e))    = decrField n f
-eval (Decr n v@(VariableRef s)) = decrVar n v
+eval (Decr n v@(VariableRef s)) = decrVar   n v
+eval (Decr n a@(ArrayRef s e))  = decrArr   n a
 
 eval (Relation op le re) = do
      l <- liftM coerceToDouble $! eval le
@@ -216,6 +223,7 @@ eval (Assignment op p v) = do
      case p of
        (FieldRef ref)     -> assignToField op ref  val
        (VariableRef name) -> assignToVar   op name val
+       (ArrayRef arr ref) -> assignToArr   op arr ref val
        otherwise -> fail "Only to-field and to-variable assignments are supported"
 
 calcNewValue oldVal op arg =
@@ -230,14 +238,14 @@ calcNewValue oldVal op arg =
 assignToField op ref val = do
      i <- liftM coerceToInt $! eval ref
      oldFields <- gets hcFields
-     let newValue  = calcNewValue (oldFields Map.! i) op val
-         newFields = Map.insert i newValue oldFields
+     let newValue  = calcNewValue (oldFields M.! i) op val
+         newFields = M.insert i newValue oldFields
      modify (\s -> s { hcFields = newFields })
      reconstructThisLine
      return $! newValue
 
 reconstructThisLine = do
-     thisFields <- gets (Map.toList . hcFields)
+     thisFields <- gets (M.toList . hcFields)
      ofs        <- liftM toString $ eval (BuiltInVar "OFS")
      let line = B.intercalate ofs $ map (toString . snd) thisFields
      modify (\s -> s { hcThisLine = line })
@@ -245,17 +253,26 @@ reconstructThisLine = do
 
 assignToVar op name val = do
      oldVars <- gets hcVars
-     let newValue = calcNewValue (oldVars Map.! name) op val
-         newVars  = Map.insert name newValue oldVars
+     let newValue = calcNewValue (oldVars M.! name) op val
+         newVars  = M.insert name newValue oldVars
      modify (\s -> s { hcVars = newVars })
      return $! newValue
 
 -- Currently for internal use only
 assignToBVar op name val = do
    oldBVars <- gets hcBVars
-   let newValue = calcNewValue (oldBVars Map.! name) op val
-       newBVars = Map.insert name newValue oldBVars
+   let newValue = calcNewValue (oldBVars M.! name) op val
+       newBVars = M.insert name newValue oldBVars
    modify $ (\s -> s { hcBVars = newBVars })
+   return $! newValue
+
+assignToArr op arr ref val = do
+   oldArrs <- gets hcArrays
+   subscr  <- liftM toString $ eval ref
+   let index     = (arr, B.unpack subscr)
+       newValue  = calcNewValue (oldArrs M.! index) op val
+       newArrays = M.insert index newValue oldArrs
+   modify $ (\s -> s { hcArrays = newArrays })
    return $! newValue
 
 incrField n fld@(FieldRef e) = do
@@ -278,6 +295,15 @@ decrVar n var@(VariableRef s) = do
    newVal <- assignToVar "-=" s (VDouble 1.0)
    return (if n == Post then oldVal else newVal)
 
+incrArr n arr@(ArrayRef name ref) = do
+   oldVal <- eval arr
+   newVal <- assignToArr "+=" name ref (VDouble 1.0)
+   return (if n == Post then oldVal else newVal)
+
+decrArr n arr@(ArrayRef name ref) = do
+   oldVal <- eval arr
+   newVal <- assignToArr "-=" name ref (VDouble 1.0)
+   return (if n == Post then oldVal else newVal)
 
 -- Coercions and conversions
 coerceToBool :: Value -> Bool
